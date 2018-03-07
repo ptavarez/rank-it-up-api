6a1d014946044b10b0df6b8516d6955324d0347e
ptavarez16 <ptavarez16@yahoo.com>
2018-03-07 16:19:26 -0500

    File name modification - Changed matches to match (singular, duh)

---
+++ scripts/matches/create-match.sh
@@ -0,0 +1,15 @@
+# DESCRIPTION="Card Game" CHALLENGER="Gabriel" OPPONENT="Renee" sh  scripts/matches/create-match.sh
+
+curl "http://localhost:4741/matches" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "match": {
+      "description": "'"${DESCRIPTION}"'",
+      "challenger": "'"${CHALLENGER}"'",
+      "opponent": "'"${OPPONENT}"'"
+    }
+  }'
+
+echo
