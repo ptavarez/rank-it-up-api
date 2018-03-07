b161cfc95c922ce650233823a6eb60874e753c6f
ptavarez16 <ptavarez16@yahoo.com>
2018-03-07 16:11:53 -0500

    Curl Request: PATCH - Successfully updated a match - End point good to go!

---
+++ scripts/matches/update-matches.sh
@@ -0,0 +1,14 @@
+# ID=3 WINNER="Renee" LOSER="Gabriel" sh  scripts/matches/update-matches.sh
+
+curl "http://localhost:4741/matches/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --data '{
+    "match": {
+      "winner": "'"${WINNER}"'",
+      "loser": "'"${LOSER}"'"
+    }
+  }'
+
+echo
