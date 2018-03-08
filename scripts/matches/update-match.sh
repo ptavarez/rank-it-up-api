7d1956c69d1a13fee43fa1e37ee6a79ffed6264f
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 09:34:02 -0500

    Token curl request: Update - Added token section to script - Successfully updated a match with an authentication token - End point good to go

--- scripts/matches/update-match.sh
+++ scripts/matches/update-match.sh
@@ -1,9 +1,10 @@
-# ID=3 WINNER="Renee" LOSER="Gabriel" sh  scripts/matches/update-match.sh
+# TOKEN="" ID= WINNER="" LOSER="" sh scripts/matches/update-match.sh
 
 curl "http://localhost:4741/matches/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "match": {
       "winner": "'"${WINNER}"'",
