85db14d94cdf6fc197467722acf097f5ebe17bec
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 09:59:33 -0500

    Matches migration - Rolled back matches migration - Updated migration so user has to input a challenger and opponent

--- db/migrate/20180307203308_create_matches.rb
+++ db/migrate/20180307203308_create_matches.rb
@@ -2,8 +2,8 @@ class CreateMatches < ActiveRecord::Migration[5.1]
   def change
     create_table :matches do |t|
       t.string :description
-      t.string :challenger
-      t.string :opponent
+      t.string :challenger, null: false
+      t.string :opponent, null: false
       t.string :winner
       t.string :loser
 
