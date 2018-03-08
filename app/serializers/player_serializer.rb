6520813e03919a6e160261e72762e9421428620e
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 15:11:44 -0500

    Player editable - Added column that states wether a player is editable or not

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,4 +1,8 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :wins, :losses
+  attributes :id, :name, :description, :wins, :losses, :editable
   belongs_to :matches
+
+  def editable
+    scope == object.user
+  end
 end
