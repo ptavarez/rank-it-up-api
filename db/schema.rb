c7094bbe2e6e74adb7080d2c766da20e10e416fa
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 14:50:29 -0500

    Add User To Players - Added user reference to players - A user has many players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308163611) do
+ActiveRecord::Schema.define(version: 20180308192310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180308163611) do
     t.integer "losses"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_players_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 20180308163611) do
 
   add_foreign_key "examples", "users"
   add_foreign_key "matches", "players"
+  add_foreign_key "players", "users"
 end
