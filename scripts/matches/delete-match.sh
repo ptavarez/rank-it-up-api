ca6037535fff51d1abe3806087c29930a63e128f
ptavarez16 <ptavarez16@yahoo.com>
2018-03-08 08:47:56 -0500

    Token curl request: Delete - Added token section to script - Successfully deleted a match with an authentication token - End point good to go!

--- scripts/matches/delete-match.sh
+++ scripts/matches/delete-match.sh
@@ -1,8 +1,9 @@
-# ID=4 sh  scripts/matches/delete-match.sh
+# TOKEN="" ID= sh  scripts/matches/delete-match.sh
 
 curl "http://localhost:4741/matches/${ID}" \
   --include \
   --request DELETE \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
 
 echo
